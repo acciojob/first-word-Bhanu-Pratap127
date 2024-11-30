49068f52b2cdf1666de97bfcbfa19d50bb85ccac
Bhanu-Pratap127 <bhanupartap1273@gmail.com>
2024-11-30 23:11:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	for (let i = 0; i < s.length; i++) {
+		if (s.charAt(i)==="") {
+			return s.substring(0,i)
+		}
+	}
+	return s
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
